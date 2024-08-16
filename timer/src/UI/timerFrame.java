4cb6f91f2ee0e7a64aced75aa8437ffff09304cd
Han Dong Woo <gksehddn8@naver.com>
2024-08-16 23:56:51 +0900

    spinner 추가

--- timer/src/UI/timerFrame.java
+++ timer/src/UI/timerFrame.java
@@ -1,41 +1,20 @@
 package UI;
+
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
 
-public class timerFrame extends JFrame {
-    public JButton startBtn; // 시작 버튼
-    public JButton resetBtn; // 초기화 버튼
-    public JLabel hourLabel; // 시간 
-    public JLabel minuteLabel; // 분 
+class LabelPanel extends JPanel {
+    public JLabel hourLabel; // 시간
+    public JLabel minuteLabel; // 분
     public JLabel secondLabel; // 초
-    public Container c = getContentPane();
 
-    public timerFrame() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        c.setLayout(null);
-        attachBtn();
+    public LabelPanel() {
+        setLayout(new GridLayout(1, 3, 50, 10));
         attachLabel();
-        setSize(600, 300);
-        setVisible(true);   
+        alignText();
     }
 
-    public void attachBtn() {
-        startBtn = new JButton("시작");
-        resetBtn = new JButton("초기화");
-       
-        // 버튼 크기 조정
-        startBtn.setSize(100, 20);
-        resetBtn.setSize(100, 20);
-        // 버튼 위치 조정
-        startBtn.setLocation(200, 200);
-        resetBtn.setLocation(300, 200);
-        // 버튼 부착
-        c.add(startBtn);
-        c.add(resetBtn);
-    }
-
-    public void attachLabel() {
+    private void attachLabel() {
         hourLabel = new JLabel("시간");
         minuteLabel = new JLabel("분");
         secondLabel = new JLabel("초");
@@ -44,22 +23,76 @@ public class timerFrame extends JFrame {
         hourLabel.setSize(50, 20);
         minuteLabel.setSize(50, 20);
         secondLabel.setSize(50, 20);
-        // label 위치 조정
-        hourLabel.setLocation(100,20);
-        minuteLabel.setLocation(300, 20);
-        secondLabel.setLocation(500, 20);
-        
-        // Border border = BorderFactory.createLineBorder(Color.BLACK, 2); // 검정색, 두께 2
-        // hourLabel.setBorder(border);
-        // hourLabel.setHorizontalAlignment(JLabel.CENTER);
-        // hourLabel.setVerticalAlignment(JLabel.CENTER);
-        // minuteLabel.setHorizontalAlignment(JLabel.CENTER);
-        // minuteLabel.setVerticalAlignment(JLabel.CENTER);
-        // secondLabel.setHorizontalAlignment(JLabel.CENTER);
-        // secondLabel.setVerticalAlignment(JLabel.CENTER);
+
         // label 부착
-        c.add(hourLabel);
-        c.add(minuteLabel);
-        c.add(secondLabel);
+        this.add(hourLabel);
+        this.add(minuteLabel);
+        this.add(secondLabel);
+    }
+
+    private void alignText() {
+        hourLabel.setHorizontalAlignment(JLabel.CENTER);
+        hourLabel.setVerticalAlignment(JLabel.CENTER);
+        minuteLabel.setHorizontalAlignment(JLabel.CENTER);
+        minuteLabel.setVerticalAlignment(JLabel.CENTER);
+        secondLabel.setHorizontalAlignment(JLabel.CENTER);
+        secondLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+}
+
+class TimePanel extends JPanel {
+    public JSpinner hourSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 23, 1));
+    public JSpinner minuteSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
+    public JSpinner secondSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
+
+    public TimePanel() {
+        setLayout(null);
+        attachSpinner();
+    }
+
+    private void attachSpinner() {
+        hourSpinner.setLocation(33, 20);
+        hourSpinner.setSize(35, 35);
+        minuteSpinner.setLocation(175, 20);
+        minuteSpinner.setSize(35, 35);
+        secondSpinner.setLocation(320, 20);
+        secondSpinner.setSize(35, 35);
+        this.add(hourSpinner);
+        this.add(minuteSpinner);
+        this.add(secondSpinner);
+    }
+}
+
+class ButtonPanel extends JPanel {
+    public JButton startBtn; // 시작 버튼
+    public JButton resetBtn; // 초기화 버튼
+
+    public ButtonPanel() {
+        attachBtn();
+    }
+
+    private void attachBtn() {
+        startBtn = new JButton("시작");
+        resetBtn = new JButton("초기화");
+
+        // 버튼 크기 조정
+        startBtn.setSize(100, 20);
+        resetBtn.setSize(100, 20);
+
+        // 버튼 부착
+        this.add(startBtn);
+        this.add(resetBtn);
+    }
+}
+
+public class timerFrame extends JFrame {
+    public timerFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.add(new LabelPanel(), BorderLayout.NORTH);
+        c.add(new TimePanel(), BorderLayout.CENTER);
+        c.add(new ButtonPanel(), BorderLayout.SOUTH);
+        setSize(400, 300);
+        setVisible(true);
     }
 }
